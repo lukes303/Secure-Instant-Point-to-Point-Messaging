b25db1b40d6dbb7abd2668266f2e3b376e95a6b1
Luke Selby <lukeselby3@gmail.com>
2025-04-30 20:17:02 -0500

    Started Message window

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from Gui import *
+from gui import *
 from AppController import *
 
 user_config = password_window()
 
 if user_config:
     initialize_app(user_config)
-    # start_network()
+    start_network()
+
+    message_window()
